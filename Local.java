e7f814dd3a6785e2b4278d763a096a1cb0cef484
Muhamad Nur Asyraaf <masyraaf14@gmail.com>
2023-06-10 22:21:16 +0800

    my part finished

--- Local.java
+++ Local.java
@@ -1,6 +1,30 @@
 public class Local extends Car{
-    
-    private tax;
 
+    private String manufactureDate;
+    private String manufactureState;
+
+    public Local(){
+        super();
+        manufactureDate = null;
+        manufactureState = null;
+    }
+    public Local(String regnum,String model,double netPrice,String date,String state){
+        super(regnum, model, netPrice);
+        manufactureDate = date;
+        manufactureState = state;
+    }
+
+    public String getManufactureDate(){return manufactureDate;}
+    public String getManufactureState(){return manufactureState;}
+
+    public double calcPrice(){
+        double tax = netPrice * 0.05;
+        double total = netPrice + tax;
+        return total;
+    }
+
+    public String toString(){
+        return super.toString() + "\nManufacturing Date : " + manufactureDate + "\nManufacturing State : " + manufactureState;
+    }
 
 }
---
+++ testCarApp.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class testCarApp {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"**********WELCOME to CAR MOLEKZ SALES RECORD SYSTEM**********");
+        
+    }
+}
